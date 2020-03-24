7871d17389311c8c7ffefc974f0188eae7e04de4
liujiquan <18242744320@163.com>
2020-03-24 21:10:00 +0800

    employee已删除

--- employee/src/main/java/com/ssm/employee/controller/MasterController.java
+++
@@ -1,9 +0,0 @@
-package com.ssm.employee.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MasterController {
-
-	
-}
--- employee/src/main/java/com/ssm/employee/service/UserInfoService.java
+++
@@ -1,7 +0,0 @@
-package com.ssm.employee.service;
-
-import com.ssm.employee.pojo.UserInfo;
-
-public interface UserInfoService {
-	public UserInfo getUserByLogin(String name,String pwd);
-}
