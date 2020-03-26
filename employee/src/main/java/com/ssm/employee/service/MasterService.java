b408a0c5a833de95a467591e51af5d4217664e2d
liujiquan <18242744320@163.com>
2020-03-26 16:51:57 +0800

    employee已删除

--- employee/src/main/java/com/ssm/employee/service/MasterService.java
+++
@@ -1,9 +0,0 @@
-package com.ssm.employee.service;
-
-import java.util.List;
-
-import com.ssm.employee.pojo.Master;
-
-public interface MasterService {
-	public List<Master> getMastersBycategoryCode(String ccode);
-}
